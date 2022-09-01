524d344b10398aa1a37d82685fd9299e434fd9c2
Lukas Reich <lukas.reich@structr.com>
2022-09-01 17:59:51 +0200

    Adds workflow for testing purposes. - Also changes structr image version to 4.3-SNAPSHOT - Adds changes to setup script that now creates a structr user group and adds the current user to the group.

---
+++ .github/workflows/healthCheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function healthCheck() {
+    echo "Waiting for Structr to be ready for deployment"
+    for i in $(seq 1 30)
+    do
+      STATUS_CODE=$(curl --write-out %{http_code} --silent localhost:8082/structr/health/ready)
+      
+      if [ "$STATUS_CODE" -ne 200 ] ; then
+        echo "Status is $STATUS_CODE. Still waiting for Structr to start..."
+        sleep 5
+      else
+        echo "Running Structr instance found"
+        return 0
+      fi
+    done
+    echo "Structr failed to start. Exiting..."
+    exit 1
+}
+
+healthCheck
+deploy
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -10,13 +10,40 @@ if ! [ -x "$(command -v docker)" ]; then
 fi
 
 SWARM_ACTIVE=`docker info | grep 'Swarm: active' -c`
+USER=$(id -un)
 
 echo "Checking docker configuration for active docker swarm mode..."
 if [ $SWARM_ACTIVE -eq 0 ];	
 	then
-   		echo "${RED}\n\tDocker swarm mode not active.\n\t${NC}Please refer to the README.md for more information.\n";
+   		echo -e "${RED}\n\tDocker swarm mode not active.\n\t${NC}Please refer to the README.md for more information.\n";
 	else
-		echo "${GREEN}\n\tDocker swarm mode active.\n\n${NC}To start Structr and Neo4j, run the following command or refer to the README.md for more information:\n\n\tdocker stack deploy --compose-file=docker-compose.yml structr\n";
+		echo -e "${GREEN}\n\tDocker swarm mode active.\n\n${NC}To start Structr and Neo4j, run the following command or refer to the README.md for more information:\n\n\tdocker stack deploy --compose-file=docker-compose.yml structr\n";
+fi
+
+echo "Checking user and group configuration..."
+if [[ $OSTYPE == 'darwin'* ]]; 
+	then
+
+		echo
+		echo "	darwin system detected we need sudo privileges to create the structr user group"
+		echo
+
+		if ! [ -n $(dscl . list /Groups | grep structr) ]; then
+
+			echo "Adding new structr user group for deployment file exchange between host and containers..."
+			sudo dscl . -create /Groups/structr gid 8082
+			sudo dseditgroup -o edit -a $USER -t user structr
+
+		fi
+		
+	else
+		if ! [ $(getent group structr) ] && [ -x "$(command -v groupadd)" ]; then
+
+			echo "Adding new structr user group for deployment file exchange between host and containers."
+			groupadd -f --gid 8082 structr		
+			usermod -a -G structr $USER
+
+		fi
 fi
 
 echo "Creating structr file volume mount"
@@ -24,6 +51,8 @@ mkdir -p ./volumes/structr-files
 
 echo "Creating structr repository volume mount"
 mkdir -p ./volumes/structr-repository
+echo "Setting group structr as owner of the structr usergroup. We need sudo access for this..."
+sudo chown -R $USER:8082 ./volumes/structr-repository
 
 echo "Creating structr logs volume mount"
 mkdir -p ./volumes/structr-logs
@@ -37,6 +66,6 @@ mkdir -p ./volumes/neo4j-logs
 mkdir -p ./structr
 touch ./structr/license.key
 
-echo "\n${GREEN}Setup complete"
+echo -e "\n${GREEN}Setup complete"
 
 exit 0
